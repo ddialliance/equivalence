e5b54c4d4fd0f9eb8ed5d63097e036e14a5d640f
Dan Smith <dan@colectica.com>
2026-01-14 17:49:06 -0600

    separate sssom tsv and yml content, for easier tsv editing and display

--- build/build.sh
+++ build/build.sh
@@ -5,5 +5,9 @@ set -e
 gen-owl -f json-ld  ddi-common.yml > generated/ddi-common.json
 gen-owl -f ttl  ddi-common.yml > generated/ddi-common.owl.ttl
 
-sssom convert mappings/lifecycle.sssom.tsv --output generated/lifecycle-mapping.owl.ttl --output-format owl
-sssom convert mappings/cdi.sssom.tsv --output generated/cdi-mapping.owl.ttl --output-format owl
+
+sssom parse -m mappings/lifecycle.sssom.yml -I tsv mappings/lifecycle.sssom.tsv --output build/work/lifecycle-merged.sssom.tsv
+sssom parse -m mappings/cdi.sssom.yml -I tsv mappings/cdi.sssom.tsv --output build/work/cdi-merged.sssom.tsv
+
+sssom convert build/work/lifecycle-merged.sssom.tsv --output generated/lifecycle-mapping.owl.ttl --output-format owl
+sssom convert build/work/cdi-merged.sssom.tsv --output generated/cdi-mapping.owl.ttl --output-format owl
